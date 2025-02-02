62d063258e1038300ad90dd6f0c9b7588b9abd75
Lakejames <156643279+Lakejames@users.noreply.github.com>
2025-02-02 20:25:26 +0700

    mengubah Background pada check dan result, menghilangkat background awal berupa connection rs232 dll

--- lib/race_result_page.dart
+++ lib/race_result_page.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_libserialport/flutter_libserialport.dart';
+import 'package:provider/provider.dart';
+import 'background_provider.dart';
 import 'dart:typed_data';
 import 'api_service.dart';
 import 'home_page.dart';
@@ -275,202 +277,159 @@ Future<void> _fetchBibDetails(String bibNumber) async {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute(builder: (context) => HomePage()),
-          (route) => false,
-        );
-        return false;
-      },
-      child: Scaffold(
-        backgroundColor: Color(0xFFCDC4C4),
-        appBar: AppBar(
-          backgroundColor: Colors.grey[200],
-          title: Row(
-            children: [
-              Image.asset(
-                'assets/logo.png',
-                height: 30,
-              ),
-              SizedBox(width: 10),
-              Text(
-                'Labsco Sport',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: Text(
-                  'RFID Tag Check',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                ),
-              ),
+    final backgroundProvider = Provider.of<BackgroundProvider>(context);
+    final File? backgroundImage = backgroundProvider.homeBannerImage;
+
+    return Scaffold(
+      backgroundColor: Color(0xFFCDC4C4),
+      appBar: AppBar(
+        backgroundColor: Colors.grey[200],
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/logo.png',
+              height: 30,
             ),
-            Container(
-              width: 200,
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _bibController,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                  hintText: "Enter BIB Number",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
+            SizedBox(width: 10),
+            Text(
+              'Labsco Sport',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-            ),
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () async {
-                String bibNumber = _bibController.text.trim();
-                _fetchBibDetails(bibNumber);
-              },
-              color: Colors.black,
-            ),
-            PopupMenuButton<String>(
-              icon: Icon(Icons.menu, color: Colors.black),
-              onSelected: (value) {
-                if (value == 'Home') {
-                  Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                    (route) => false,
-                  );
-                } else if (value == 'RFID Tag Check') {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => RFIDTagCheckPage()),
-                  );
-                } else if (value == 'Race Result') {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => RaceResultPage()),
-                  );
-                } else if (value == 'Upload Excel') {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ExcelUploadPage()),
-                  );
-                } else if (value == 'Display Settings') {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ChangeBackgroundPage()),
-                  );
-                }
-              },
-              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                PopupMenuItem<String>(
-                  value: 'Home',
-                  child: ListTile(
-                    leading: Icon(Icons.home),
-                    title: Text('Home'),
-                  ),
-                ),
-                PopupMenuItem<String>(
-                  value: 'RFID Tag Check',
-                  child: ListTile(
-                    leading: Icon(Icons.info),
-                    title: Text('RFID Tag Check'),
-                  ),
-                ),
-                PopupMenuItem<String>(
-                  value: 'Race Result',
-                  child: ListTile(
-                    leading: Icon(Icons.insert_chart_outlined_outlined),
-                    title: Text('Race Result'),
-                  ),
-                ),
-                PopupMenuItem<String>(
-                  value: 'Upload Excel',
-                  child: ListTile(
-                    leading: Icon(Icons.upload_file),
-                    title: Text('Upload Excel'),
-                  ),
-                ),
-                PopupMenuItem<String>(
-                  value: 'Display Settings',
-                  child: ListTile(
-                    leading: Icon(Icons.settings),
-                    title: Text('Display Settings'),
-                  ),
-                ),
-              ],
             ),
           ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.wifi_tethering,
-                size: 100,
-                color: Colors.black54,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
+                'RFID Race Result Check',
+                style: TextStyle(color: Colors.black, fontSize: 16),
               ),
-              SizedBox(height: 20),
-              Text(
-                connectionStatus,
-                style: TextStyle(
-                  fontSize: 18,
-                  color: connectionStatus == "Terhubung ke RFID scanner"
-                      ? Colors.green
-                      : Colors.red,
+            ),
+          ),
+          Container(
+            width: 200,
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _bibController,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                hintText: "Enter BIB Number",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
                 ),
               ),
-              SizedBox(height: 20),
-              Text(
-                'Data RFID (Hex): $rfidData',
-                style: TextStyle(fontSize: 16, color: Colors.black),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              String bibNumber = _bibController.text.trim();
+              _fetchBibDetails(bibNumber);
+            },
+            color: Colors.black,
+          ),
+          PopupMenuButton<String>(
+            icon: Icon(Icons.menu, color: Colors.black),
+            onSelected: (value) {
+              if (value == 'Home') {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                  (route) => false,
+                );
+              } else if (value == 'RFID Tag Check') {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RFIDTagCheckPage()),
+                );
+              } else if (value == 'Race Result') {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RaceResultPage()),
+                );
+              } else if (value == 'Upload Excel') {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ExcelUploadPage()),
+                );
+              } else if (value == 'Display Settings') {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ChangeBackgroundPage()),
+                );
+              }
+            },
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+              PopupMenuItem<String>(
+                value: 'Home',
+                child: ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text('Home'),
+                ),
               ),
-              SizedBox(height: 10),
-              Text(
-                'Filtered Data: $filteredRfidData',
-                style: TextStyle(fontSize: 16, color: Colors.black),
+              PopupMenuItem<String>(
+                value: 'RFID Tag Check',
+                child: ListTile(
+                  leading: Icon(Icons.info),
+                  title: Text('RFID Tag Check'),
+                ),
               ),
-              SizedBox(height: 20),
-              DropdownButton<String>(
-                value: _selectedPort,
-                items: _availablePorts.map((String port) {
-                  return DropdownMenuItem<String>(
-                    value: port,
-                    child: Text(port),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _selectedPort = newValue;
-                  });
-                },
-                hint: Text('Select COM Port'),
+              PopupMenuItem<String>(
+                value: 'Race Result',
+                child: ListTile(
+                  leading: Icon(Icons.insert_chart_outlined_outlined),
+                  title: Text('Race Result'),
+                ),
               ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: _configureSerialPort,
-                child: Text('Connect RS232'),
+              PopupMenuItem<String>(
+                value: 'Upload Excel',
+                child: ListTile(
+                  leading: Icon(Icons.upload_file),
+                  title: Text('Upload Excel'),
+                ),
               ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: _toggleConnectionType,
-                child: Text(_currentConnectionType == ConnectionType.RS232
-                    ? 'Switch to TCP/IP'
-                    : 'Switch to RS232'),
+              PopupMenuItem<String>(
+                value: 'Display Settings',
+                child: ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text('Display Settings'),
+                ),
               ),
             ],
           ),
-        ),
+        ],
       ),
-    );
-  }
+      body: Container(
+        margin: EdgeInsets.all(20),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: backgroundImage != null
+              ? DecorationImage(
+                  image: FileImage(backgroundImage),
+                  fit: BoxFit.cover,
+                )
+              : null,
+          color:
+              backgroundImage == null ? Colors.grey[300] : Colors.transparent,
+        ),
+        child: backgroundImage == null
+            ? Center(
+                child: Text(
+                  'No Background Available',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              )
+            : null,
+   ),
+ );
+}
 
   void _showErrorDialog(String message) {
     showDialog(
